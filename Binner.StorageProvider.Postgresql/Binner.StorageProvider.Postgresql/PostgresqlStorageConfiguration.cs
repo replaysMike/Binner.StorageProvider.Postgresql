dd576ec63290cc3fa9e8c12180e4c4c210066e67
Michael Brown <mikeb@360replays.com>
2022-01-10 14:53:00 -0800

    Initial commit

---
+++ Binner.StorageProvider.Postgresql/Binner.StorageProvider.Postgresql/PostgresqlStorageConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace Binner.StorageProvider.Postgresql
+{
+    internal class PostgresqlStorageConfiguration
+    {
+        public string ConnectionString { get; set; }
+
+        public PostgresqlStorageConfiguration()
+        {
+        }
+
+        public PostgresqlStorageConfiguration(IDictionary<string, string> config)
+        {
+            if (config.ContainsKey("ConnectionString"))
+                ConnectionString = config["ConnectionString"];
+        }
+    }
+}
